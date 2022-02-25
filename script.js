2865df07f75e5921eacc168af63337d6c6fc07fc
Match007007 <match007007@gmail.com>
2022-02-25 15:25:37 +0800

    little change

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-'use strict';
+"use strict";
 // Selecting elements
-const PLayer0L = document.querySelector('.player--0');
-const PLayer1L = document.querySelector('.player--1');
+const PLayer0L = document.querySelector(".player--0");
+const PLayer1L = document.querySelector(".player--1");
 
-const score0L = document.querySelector('#score--0'); //when slecting elements, . is used for selecting class, # is used for selecting id.
-const score1L = document.getElementById('score--1'); // the above works the same as this
-const diceE1 = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
-const Current0L = document.getElementById('current--0');
-const Current1L = document.getElementById('current--1');
+const score0L = document.querySelector("#score--0"); //when slecting elements, . is used for selecting class, # is used for selecting id.
+const score1L = document.getElementById("score--1"); // the above works the same as this
+const diceE1 = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+const Current0L = document.getElementById("current--0");
+const Current1L = document.getElementById("current--1");
 
 let score, CurrentScore, ActivePlayer, playing;
 
 const restart = function () {
-  score = [0,0];
+  score = [0, 0];
   CurrentScore = 0;
   ActivePlayer = 0;
   playing = true;
@@ -25,11 +25,11 @@ const restart = function () {
   Current0L.textContent = 0;
   Current1L.textContent = 0;
 
-  diceE1.classList.add('hidden');
-  PLayer0L.classList.remove('player--winner');
-  PLayer1L.classList.remove('player--winner');
-  PLayer0L.classList.add('player--active');
-  PLayer1L.classList.remove('player--active');
+  diceE1.classList.add("hidden");
+  PLayer0L.classList.remove("player--winner");
+  PLayer1L.classList.remove("player--winner");
+  PLayer0L.classList.add("player--active");
+  PLayer1L.classList.remove("player--active");
 };
 
 restart();
@@ -39,22 +39,22 @@ const switchPlayer = function () {
   document.getElementById(`current--${ActivePlayer}`).textContent =
     CurrentScore;
   ActivePlayer = ActivePlayer === 0 ? 1 : 0;
-  PLayer0L.classList.toggle('player--active');
-  PLayer1L.classList.toggle('player--active');
+  PLayer0L.classList.toggle("player--active");
+  PLayer1L.classList.toggle("player--active");
 };
 
 // startingconditions
 
-diceE1.classList.add('hidden');
+diceE1.classList.add("hidden");
 
 //New Game Funtionality
 
 //Rolling Dice Funcationality
-btnRoll.addEventListener('click', function () {
+btnRoll.addEventListener("click", function () {
   if (playing) {
     const RolledDice = Math.trunc(Math.random() * 6) + 1; //generating a random dice number
 
-    diceE1.classList.remove('hidden'); // remove the hidden and make the dice happen again
+    diceE1.classList.remove("hidden"); // remove the hidden and make the dice happen again
     diceE1.src = `dice-${RolledDice}.png`; // change src to the picture information name
 
     //check for rolled 1:
@@ -71,7 +71,7 @@ btnRoll.addEventListener('click', function () {
 });
 //button functionary
 
-btnHold.addEventListener('click', function () {
+btnHold.addEventListener("click", function () {
   if (playing) {
     //Add cuurent score to the active payer's score
     score[ActivePlayer] += CurrentScore;
@@ -81,34 +81,18 @@ btnHold.addEventListener('click', function () {
     //check if the sore >=100
     if (score[ActivePlayer] >= 100) {
       playing = false;
-      diceE1.classList.remove('hidden');
+      diceE1.classList.remove("hidden");
 
       document
         .querySelector(`.player--${ActivePlayer}`)
-        .classList.add('player--winner');
+        .classList.add("player--winner");
       document
         .querySelector(`.player--${ActivePlayer}`)
-        .classList.remove('player--active');
+        .classList.remove("player--active");
     } else {
       switchPlayer();
     }
   }
 });
 
-btnNew.addEventListener('click', restart); // you put the const which is a function there as the augurment so dont need to declare it with  restart ()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+btnNew.addEventListener("click", restart); // you put the const which is a function there as the augurment so dont need to declare it with  restart ()
