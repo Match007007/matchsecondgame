29c8ca105c2dd9047d906fe87734ba63c8144902
Match007007 <match007007@gmail.com>
2022-02-25 16:29:29 +0800

    change
    change

--- script.js
+++ script.js
@@ -1,98 +1,94 @@
 'use strict';
-
 // Selecting elements
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
-const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
+const PLayer0L = document.querySelector('.player--0');
+const PLayer1L = document.querySelector('.player--1');
 
-const diceEl = document.querySelector('.dice');
+const score0L = document.querySelector('#score--0'); //when slecting elements, . is used for selecting class, # is used for selecting id.
+const score1L = document.getElementById('score--1'); // the above works the same as this
+const diceE1 = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const Current0L = document.getElementById('current--0');
+const Current1L = document.getElementById('current--1');
 
-let scores, currentScore, activePlayer, playing;
+let score, CurrentScore, ActivePlayer, playing;
 
-// Starting conditions
-const init = function () {
-  scores = [0, 0];
-  currentScore = 0;
-  activePlayer = 0;
+const restart = function () {
+  score = [0, 0];
+  CurrentScore = 0;
+  ActivePlayer = 0;
   playing = true;
 
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
+  score0L.textContent = 0;
+  score1L.textContent = 0;
+  Current0L.textContent = 0;
+  Current1L.textContent = 0;
 
-  diceEl.classList.add('hidden');
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
+  diceE1.classList.add('hidden');
+  PLayer0L.classList.remove('player--winner');
+  PLayer1L.classList.remove('player--winner');
+  PLayer0L.classList.add('player--active');
+  PLayer1L.classList.remove('player--active');
 };
-init();
+
+restart();
 
 const switchPlayer = function () {
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-  currentScore = 0;
-  activePlayer = activePlayer === 0 ? 1 : 0;
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
+  CurrentScore = 0;
+  document.getElementById(`current--${ActivePlayer}`).textContent =
+    CurrentScore;
+  ActivePlayer = ActivePlayer === 0 ? 1 : 0;
+  PLayer0L.classList.toggle('player--active');
+  PLayer1L.classList.toggle('player--active');
 };
 
-// Rolling dice functionality
+//New Game Funtionality
+
+//Rolling Dice Funcationality
 btnRoll.addEventListener('click', function () {
   if (playing) {
-    // 1. Generating a random dice roll
-    const dice = Math.trunc(Math.random() * 6) + 1;
+    const RolledDice = Math.trunc(Math.random() * 6) + 1; //generating a random dice number
 
-    // 2. Display dice
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
+    diceE1.classList.remove('hidden'); // remove the hidden and make the dice happen again
+    diceE1.src = `dice-${RolledDice}.png`; // change src to the picture information name
 
-    // 3. Check for rolled 1
-    if (dice !== 1) {
-      // Add dice to current score
-      currentScore += dice;
-      document.getElementById(
-        `current--${activePlayer}`
-      ).textContent = currentScore;
-    } else {
-      // Switch to next player
+    //check for rolled 1:
+    if (RolledDice !== 1) {
+      //add dice number to current score
+      CurrentScore += RolledDice; //add dice number to current score
+      document.getElementById(`current--${ActivePlayer}`).textContent =
+        CurrentScore; //display current score to the webpage , changre later
+    } // switching the user
+    else {
       switchPlayer();
     }
   }
 });
+//button functionary
 
 btnHold.addEventListener('click', function () {
   if (playing) {
-    // 1. Add current score to active player's score
-    scores[activePlayer] += currentScore;
-    // scores[1] = scores[1] + currentScore
+    //Add cuurent score to the active payer's score
+    score[ActivePlayer] += CurrentScore;
+    document.getElementById(`score--${ActivePlayer}`).textContent =
+      score[ActivePlayer];
 
-    document.getElementById(`score--${activePlayer}`).textContent =
-      scores[activePlayer];
-
-    // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 100) {
-      // Finish the game
+    //check if the sore >=100
+    if (score[ActivePlayer] >= 100) {
       playing = false;
-      diceEl.classList.add('hidden');
+      diceE1.classList.remove('hidden');
 
       document
-        .querySelector(`.player--${activePlayer}`)
+        .querySelector(`.player--${ActivePlayer}`)
         .classList.add('player--winner');
       document
-        .querySelector(`.player--${activePlayer}`)
+        .querySelector(`.player--${ActivePlayer}`)
         .classList.remove('player--active');
     } else {
-      // Switch to the next player
       switchPlayer();
     }
   }
 });
 
-btnNew.addEventListener('click', init);
+btnNew.addEventListener('click', restart); // you put the const which is a function there as the augurment so dont need to declare it with  restart ()
